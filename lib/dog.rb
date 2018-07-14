38abf28c7ccc14c154d1698a04886f885b75576d
katiejg <jng.katie@gmail.com>
2018-07-14 15:39:34 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,10 @@
 # dog.rb
+
+class Dog 
+attr_accessor :name, :breed
+end
+
+fido = Dog.new
+fido.name = "Fido"
+snoopy = Dog.new
+snoopy.breed = "beagle"
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,8 @@
 # person.rb
+
+class Person
+attr_accessor :name, :job
+end
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
